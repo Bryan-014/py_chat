65ec6d7cc6134031f6003e67de772055b341341c
Bryan-014 <85651446+Bryan-014@users.noreply.github.com>
2025-05-08 10:00:15 -0300

    Add files via upload

---
+++ udp/client.py
@@ -0,0 +1,20 @@
+import socket
+
+def send_message(host: str, port: int):
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.settimeout(1)
+    
+    
+    while True:
+        message = input('type your message: ')
+        server.sendto(message.encode(), (host, port))
+        
+        data, addr = server.recvfrom(1024)
+        print(f'[Message]: {data.decode()}')
+        
+
+if __name__ == '__main__':
+    HOST = "localhost"
+    PORT = 8000
+    
+    send_message(HOST, PORT)
