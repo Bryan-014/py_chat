567d851525e269832710190723a2b7e5e3f8178c
Bryan-014 <bryangustavo201904@gmail.com>
2025-05-15 06:09:31 -0300

    Thread

--- udp/client.py
+++ udp/client.py
@@ -1,18 +1,32 @@
 import socket
+import threading
+
+def receive_messages(sock):
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            print(f"\n[{addr}]: {data.decode()}")
+        except:
+            continue
 
 def send_message(host: str, port: int):
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.settimeout(1)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.bind(('', 0))
+    client.settimeout(1)
+
+    listener = threading.Thread(target=receive_messages, args=(client,), daemon=True)
+    listener.start()
+
+    print("Conectado ao chat. Pressione Ctrl+C para sair.")
     
-    while True:        
-        message = input('type your message: ')                    
-        server.sendto(message.encode(), (host, port))
-        
-        data, addr = server.recvfrom(1024)
-        print(f'[{addr}]: {data.decode()}')            
+    try:
+        while True:
+            msg = input()
+            client.sendto(msg.encode(), (host, port))
+    except KeyboardInterrupt:
+        print("\nSaindo do chat.")
 
 if __name__ == '__main__':
     HOST = "localhost"
     PORT = 8000
-    
     send_message(HOST, PORT)
--- udp/server.py
+++ udp/server.py
@@ -2,27 +2,25 @@ import socket
 
 def start_server(host: str, port: int):
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
     server.bind((host, port))
-    print(f"Server started at {host}:{port}\n Listening now...")
-    
+    print(f"Server started at {host}:{port}\nListening now...")
+
     clients = set()
-    
+
     while True:
         data, addr = server.recvfrom(1024)
-        print(f"[CLIENTE] {addr}: {data.decode()}")
-        
+        print(f"[{addr}]: {data.decode()}")
+
         if addr not in clients:
             clients.add(addr)
-        
+
         for client in clients:
             if client != addr:
-                server.sendto(data.encode(), (host, 8800))
+                server.sendto(f"[{addr}] {data.decode()}".encode(), client)
             else:
-                server.sendto("Mensagem Enviada".encode(), addr)
+                server.sendto("Mensagem Enviada".encode(), client)
 
 if __name__ == '__main__':
     HOST = "localhost"
     PORT = 8000
-    
     start_server(HOST, PORT)
